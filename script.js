7ed6894637682bdc08d8b7b17515900386a7202f
Your Name <you@example.com>
2024-05-09 15:41:16 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// let firstName = 'Noman';
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+
+let javascriptIsFun = true
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 34);
+console.log(typeof 'noman');
+
+javascriptIsFun = 'yes!';
+console.log(typeof javascriptIsFun);
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 199;
+console.log(typeof year);
+console.log(typeof year);
+console.log(typeof null);
+
+let age = 30;
+age = 31;
+
+const birthYear = 2002;
+// birthYear = 2003;
+
+var job = 'techer';
+job = 'student';
+
+Lastname = 'noman';
+console.log('noman');
+
+Lastname = 'noman';
+console.log('noman');
+
+const now = 2020;
+const noman = 2037 - 2002;
+const noman1 = now - 2024;
+const noman2 = 2037 - 2007;
+console.log(noman);
+console.log(noman1);
+console.log(noman2);
+
+
+age = 15;
+if (age >= 18) {
+    console.log('sara can driving');
+}
+else{
+    const yearsLeft = 18 - age;
+    console.log(`sara cannot driving is to yong. Wait another ${yearsLeft} years :)`);
+}
